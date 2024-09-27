d3766dd1f9bfbf12b6ba20a67d2bbdcbca4ca4a5
jensen <88981153+jensen-1217@users.noreply.github.com>
2024-09-27 22:20:32 +0800

    Add files via upload

---
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -0,0 +1,32 @@
+package cn.itcast.order.service;
+
+import cn.itcast.feign.clients.UserClient;
+import cn.itcast.feign.pojo.User;
+import cn.itcast.order.mapper.OrderMapper;
+import cn.itcast.order.pojo.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderMapper orderMapper;
+    @Autowired
+    private UserClient userClient;
+
+//    @Autowired
+//    private RestTemplate restTemplate;
+
+    public Order queryOrderById(Long orderId) {
+        // 1.查询订单
+        Order order = orderMapper.findById(orderId);
+//        Long userId = order.getUserId();
+//        String url ="http://userservice/user/"+userId;
+//        User user = restTemplate.getForObject(url, User.class);
+        User user = userClient.findById(order.getUserId());
+        order.setUser(user);
+        // 4.返回
+        return order;
+    }
+}
